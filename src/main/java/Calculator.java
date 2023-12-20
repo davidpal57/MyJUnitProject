05e401d9b076795297e89eab8644519d25d252ac
davidpal57 <paldavid02@gmail.com>
2023-12-20 21:02:35 +0200

    Maven

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public int add(int x, int y){
+        return x+y;
+    }
+    public int diff(int x, int y){
+        return x-y;
+    }
+    public int prod(int x, int y){
+        return x*y;
+    }
+    public double div(double x, double y){
+        return x/y;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+
+public class CalculatorTest {
+    private static Calculator calculator;
+    @BeforeAll
+    public static void beforeAllMethod(){
+        calculator = new Calculator();
+        System.out.println("BeforeAll");
+    }
+    @BeforeEach
+    public void beforeEachMethod() {
+        System.out.println("BeforeEach");
+    }
+    @Test
+    public void addTest(){
+        //Calculator calculator = new Calculator();
+        int result = calculator.add(2,3);
+        Assertions.assertEquals(result, 5);
+    }
+
+    @Test
+    public void diffTest(){
+        //Calculator calculator = new Calculator();
+        int result = calculator.diff(3,2);
+        Assertions.assertEquals(result, 1);
+    }
+
+    @Test
+    public void prodTest(){
+        //Calculator calculator = new Calculator();
+        int result = calculator.prod(5,7);
+        Assertions.assertEquals(result, 35);
+    }
+
+    @Test
+    public void divTest(){
+        Calculator calculator = new Calculator();
+        double result = calculator.div(10,5);
+        Assertions.assertEquals(result, 2);
+    }
+
+    @AfterAll
+    public static void afterAllMethod(){
+        System.out.println("AfterAll");
+    }
+    @AfterEach
+    public void afterEachMethod(){
+        System.out.println("AfterEach");
+    }
+}
